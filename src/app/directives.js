e1f74d75a4ed925c4936c80a098d5664a041aa58
Robbie Hickey <robbie.hickey@thomsonreuters.com>
2014-07-21 11:53:43 -0500

    refactored add button and extracted directives to their own file

---
+++ src/app/directives.js
@@ -0,0 +1,33 @@
+﻿(function(ngMongo) {
+
+    ngMongo.directive("deleteButton", function () {
+        return {
+            //default is attribute
+            restrict: "E",
+            //replace the html content
+            replace: true,
+            //NOTE: this is not the scope itself! It is a definition object that tells the injected scope what to do.
+            scope: {
+                // tell angular to peel text attribute value and set a variable named 'text' on the scope
+                text: "@",
+                // & tells angular to delegate whatefver the setting is to the parent scope
+                action: "&",
+            },
+            // notice we are setting the ng-click to action, which is a delegate that is inherited from the parent scope.
+            // this allows us to decouple our directive from our controller. 
+            template: "<button class='btn btn-danger' ng-click='action()'><span class='glyphicon glyphicon-remove'></span> {{text}}</button>"
+        }
+    });
+
+    ngMongo.directive("addButton", function () {
+        return {
+            restrict: "E",
+            scope: {
+                text: "@",
+                action: "&"
+            },
+            template: "<button class='btn btn-success' ng-click='addDb()'> {{text}}</button>"
+        }
+    });
+
+}(angular.module("ngMongo")));
